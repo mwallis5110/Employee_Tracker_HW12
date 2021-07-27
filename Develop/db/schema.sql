74a40a024f212ebdcfc873ad35c9c48e1e4d33db
Mason <mwallis5110@gmail.com>
2021-07-27 15:03:03 -0500

    Pushing sql tables

---
+++ Develop/db/schema.sql
@@ -0,0 +1,26 @@
+DROP DATABASE IF EXISTS employees_db;
+CREATE DATABASE employees_db;
+
+USE employees_db;
+
+CREATE TABLE departments (
+    id INT NOT NULL AUTO_INCREMENT,
+    name VARCHAR(30)
+);
+
+CREATE TABLE roles (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(30),
+    salary DECIMAL,
+    department_id INT NOT NULL,
+    FOREIGN KEY (department_id),
+    REFERENCES department(id),
+    ON DELETE SET NULL
+);
+
+CREATE TABLE employees (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    first_name VARCHAR(30),
+    last_name VARCHAR(30),
+    role_id INT NOT NULL,
+)
