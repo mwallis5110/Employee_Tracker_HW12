e08359cc595537a9d7cf18b8fd61cebc42e46543
Mason <mwallis5110@gmail.com>
2021-08-16 21:12:32 -0500

    updated sql files

--- Develop/db/schema.sql
+++ Develop/db/schema.sql
@@ -13,9 +13,7 @@ CREATE TABLE roles (
     title VARCHAR(30),
     salary DECIMAL,
     department_id INT NOT NULL,
-    FOREIGN KEY (department_id),
-    REFERENCES department(id),
-    ON DELETE SET NULL
+    FOREIGN KEY (department_id) REFERENCES department(id) ON DELETE SET NULL
 );
 
 CREATE TABLE employees (
@@ -23,5 +21,7 @@ CREATE TABLE employees (
     first_name VARCHAR(30),
     last_name VARCHAR(30),
     role_id INT NOT NULL,
-    manager_id INT NOT NULL
+    manager_id INT NOT NULL,
+    FOREIGN KEY (role_id) REFERENCES role(id) ON DELETE SET NULL,
+    FOREIGN KEY (manager_id) REFERENCES employee(id) ON DELETE SET NULL
 );
--- Develop/db/seeds.sql
+++ Develop/db/seeds.sql
@@ -1,3 +1,7 @@
 INSERT INTO departments (id, name)
+VALUES (1, "Mason Wallis"),
+       (2, "Justin Reese"),
+       (3, "Thomas Limmer"),
+       (4, "Christyn Garcia"),
+       (5, "Glen Hood");
 
-)
